a3899a1ec246d68338daa42450164b326b2a1467
Simon <sstur@me.com>
2019-12-26 21:37:01 -0800

    Backend: Express and Hello World

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,2 +1,13 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express from 'express';
+
+let app = express();
+let port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello world.');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://127.0.0.1:${port}`);
+});
